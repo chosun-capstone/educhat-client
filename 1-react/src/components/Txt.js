5fa081db91fa9330e9958bc785385faa925223ac
geuneva <sharesugi@gmail.com>
2024-05-23 19:40:06 +0900

    eva change

--- 1-react/src/components/Txt.js
+++ 1-react/src/components/Txt.js
@@ -12,7 +12,7 @@ import { Document, Page, pdfjs } from 'react-pdf';
 
 
 
-// PDF.js worker 경로 설정
+// PDF.js worker 경로
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const Txt = () => {

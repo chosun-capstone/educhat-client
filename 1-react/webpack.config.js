1cb71ea8c78e6e39557beb1c9ae372ad7ab29e2d
송영민 (YeongMin Song) <jakgon@naver.com>
2024-06-14 16:50:39 +0900

    Update webpack.config.js

--- 1-react/webpack.config.js
+++ 1-react/webpack.config.js
@@ -6,6 +6,10 @@ module.exports = {
   entry: {
     main: "./src/main.js",
   },
+  output: {
+	  filename: "[name].js", // [app].js (엔트리의 키가 app이므로)
+	  path: path.resolve(__dirname, "dist")
+  },
   devtool: "inline-source-map",
   devServer: {
     contentBase: "./",
@@ -23,5 +27,19 @@ module.exports = {
       },
     ],
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+	new HtmlWebpackPlugin({
+			// 빌드시 html에 값을 전달, html 변경 & html 코드 압축(공백, 주석제거) & webpack으로 빌드한 번들 파일을 자동으로 html에 추가
+		template: "./src/index.html", // index.html 파일을 템플릿으로 씁니다.
+		templateParameters: {
+				// 해당 템플릿에 전달하는 인자들
+		env: "",
+		},
+		minify: {
+						collapseWhitespace: true,
+						removeComments: true,
+					},
+		}),
+    new CleanWebpackPlugin()
+  ],
 };
